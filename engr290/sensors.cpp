8014df513092cc90c67fa3d88ec5aa9b556d2cc4
sambesse <72715111+sambesse@users.noreply.github.com>
2022-11-18 17:43:28 -0500

    beginning to code sensor init, working more on TWI

--- engr290/sensors.cpp
+++ engr290/sensors.cpp
@@ -1,3 +1,12 @@
 void initIMU() {
-  
+  uint8_t configString [3] = {0x01, GYRO_250, ACCEL_2G}; //set DLPF on, gyro to +-250 and accel to +-2g
+  initTWI(IMU_ADDR);
+  writeTWI(CONFIG_ADDR, configString, 3); //TODO: need to make this function for burst write, already taken care of in ISR
+}
+
+void initIRSensors() {
+  DDRC &= ~(1 << 3);
+  //TODO set other IR pin as input as well
+  ADMUX = 0x03;
+  ADCSRA = 0x88; //enable ADC with interrupt
 }
--- engr290/sensors.h
+++ engr290/sensors.h
@@ -1,5 +1,24 @@
 //sensor header, includes the sampling of all distance sensors and IMU
-
+#define IMU_ADDR 0x68 
+#define CONFIG_ADDR 0x1A
+#define GYRO_CONFIG_ADDR 0x1B
+#define ACCEL_CONFIG_ADDR 0x1C
+#define GYRO_OUT_START 0x43
+#define ACCEL_OUT_START 0x3B
+#define GYRO_250 0x00
+#define GYRO_500 0x08
+#define GYRO_1000 0x10
+#define GYRO_2000 0x18
+#define GYRO_XST 0x80
+#define GYRO_YST 0x40
+#define GYRO_ZST 0x20
+#define ACCEL_2G 0x00
+#define ACCEL_4G 0x08
+#define ACCEL_8G 0x10
+#define ACCEL_16G 0x18
+#define ACCEL_XST 0x80
+#define ACCEL_YST 0x40
+#define ACCEL_ZST 0x20
 //init function for IMU
 void initIMU();
 
@@ -14,3 +33,6 @@ uint32_t getFrontDistance();
 
 //return the angular velocity in the yaw axis as int16_t
 int16_t getYawRate();
+
+//initiailze ADC needed for reading IR sensors
+void initIRSensors();
